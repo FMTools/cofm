2e7043a794ece1902073860df990c0bef5c84788
yili.org <yili.org@9e5ccdf6-7fdb-11de-8688-af8986fd7f98>
2009-08-08 18:30:23 +0000

    Employed log4j.

--- trunk/cofm/server/src/collab/data/test/FeatureVoteTest.java
+++ trunk/cofm/server/src/collab/data/test/FeatureVoteTest.java
@@ -1,14 +1,18 @@
 package collab.data.test;
 
 import static org.junit.Assert.*;
+
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import collab.data.bean.*;
 public class FeatureVoteTest {
 	
+	static Logger logger = Logger.getLogger(FeatureVoteTest.class);
+	
 	@Test
 	public void testNormalVote() {
-		
+		logger.info("test normal");
 	}
 	
 	@Test
